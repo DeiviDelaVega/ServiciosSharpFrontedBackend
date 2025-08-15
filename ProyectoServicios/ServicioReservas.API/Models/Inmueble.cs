0b99d8f516e8dcdaffe1f1ff5b5d324ee5ad1959
GiacomoDel <giacomo.delgado@acchiwan.pe>
2025-08-14 21:49:33 -0500

    MantResservas
    Falta afinar detalles como mostrar los nombres de inmubles y clientes  en lugar de los IDs, mejrorar la paginación, y afinar aspectos visuales que dependen de la api de inmuebles

---
+++ ProyectoServicios/ServicioReservas.API/Models/Inmueble.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServicioReservas.API.Models
+{
+    public class Inmueble
+    {
+        [Key]
+        public int ID_Inmueble { get; set; }
+        public string Nombre { get; set; }
+        public int Capacidad { get; set; }
+        public int NumeroHabitaciones { get; set; }
+        public string Descripcion { get; set; }
+        public string ServiciosIncluidos { get; set; }
+        public string Disponibilidad { get; set; }
+        public decimal PrecioPorNoche { get; set; }
+        public string ImagenHabitacion { get; set; }
+        public decimal? Latitud { get; set; }
+        public decimal? Longitud { get; set; }
+    }
+}
---
+++ ProyectoServicios/ServicioReservas.API/Models/Reserva.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServicioReservas.API.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int ID_Solicitud { get; set; }
+
+        public DateTime Fecha_Solicitud { get; set; }
+
+        public int ID_Cliente { get; set; }
+        public int ID_Inmueble { get; set; }
+
+        public DateTime Fecha_Inicio_Reserva { get; set; }
+        public DateTime Fecha_Fin_Reserva { get; set; }
+        public string Metodo_Pago { get; set; }
+        public decimal Monto_Total { get; set; }
+        public string Estado_Reserva { get; set; }
+
+        // Elimina las propiedades de navegación
+        // public Cliente Cliente { get; set; }
+        // public Inmueble Inmueble { get; set; }
+    }
+}
